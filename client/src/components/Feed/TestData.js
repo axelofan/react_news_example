b08137ce2c48e53fe1e84d624b743c5dd0a9784c
axelofan <cabanov.alexey@yandex.ru>
2016-12-01 20:00:09 +0400

    Добавление элемента
    Добавление кнопки лайка и комментария
    Добавление Moment.js
    Добавление времени публикации и имени пользователя.

--- client/src/components/Feed/TestData.js
+++ client/src/components/Feed/TestData.js
@@ -1,53 +1,79 @@
-/*{
+/*news: {
     _id:1, //MongoId
-    userId:1, //MongoId
-    pubDate: 12345, //Date
-    enclosure: "persik.png", //Image URL
+    id_user:1, //MongoId
     title: "Заголовок", //String
-    description: "Текст новости", //String
-    likeCount: 999, //Int Number
-    isLiked: true //Boolean
+    date_public: 12345, //Unix Time Stamp
+    img_url: "persik.png", //Image URL
+    description: "Краткий текст новости", //String
+    text: "Полный текст новости", //String
+    keywords: ["Бугагашка","Ололошка"], //Array of String
+    is_moderate: false, //Boolean
+    like_count: 999, //Int Number
+    is_like: true //Boolean
+}
+user: {
+    name: "axelofan", //String
 }*/
 const news = [
     {   
         _id: '91caecb02aa7e28e67e17b2e',
         title: 'Блогеры переоделись в ниндзя, чтобы раздать подарки бездомным',
         description: 'Видеоблогер Райан Хига и его друзья переоделись в ниндзя, чтобы раздать подарки бездомным в Лас-Вегасе. Они незаметно оставляли подарочные пакеты с едой, теплой одеждой и одеялами рядом с бездомными.',
-        enclosure: 'https://meduza.io/image/attachments/images/000/041/438/small/ZJ9sLP2Jf_vtGI2EsgDl2g.jpg',
-        likeCount: 35,
-        isLiked:true
+        img_url: 'https://meduza.io/image/attachments/images/000/041/438/small/ZJ9sLP2Jf_vtGI2EsgDl2g.jpg',
+        like_count: 35,
+        is_like:true,
+        date_public:1480599044976,
+        user: {
+            name: "axelofan"
+        }
     },
     {   
         _id: '627073ba172e23b1e51cf768',
         title: 'Блогеры переоделись в ниндзя, чтобы раздать подарки бездомным',
         description: 'Видеоблогер Райан Хига и его друзья переоделись в ниндзя, чтобы раздать подарки бездомным в Лас-Вегасе. Они незаметно оставляли подарочные пакеты с едой, теплой одеждой и одеялами рядом с бездомными.',
-        enclosure: 'https://meduza.io/image/attachments/images/000/041/438/small/ZJ9sLP2Jf_vtGI2EsgDl2g.jpg',
-        likeCount: 48,
-        isLiked:false
+        img_url: 'https://meduza.io/image/attachments/images/000/041/438/small/ZJ9sLP2Jf_vtGI2EsgDl2g.jpg',
+        like_count: 48,
+        is_like:false,
+        date_public:1480494325000,
+        user: {
+            name: "axelofan"
+        }
     },
     {   
         _id: '6f3f6aa4f7b302bf6f4139bf',
         title: 'Блогеры переоделись в ниндзя, чтобы раздать подарки бездомным',
         description: 'Видеоблогер Райан Хига и его друзья переоделись в ниндзя, чтобы раздать подарки бездомным в Лас-Вегасе. Они незаметно оставляли подарочные пакеты с едой, теплой одеждой и одеялами рядом с бездомными.',
-        enclosure: 'https://meduza.io/image/attachments/images/000/041/438/small/ZJ9sLP2Jf_vtGI2EsgDl2g.jpg',
-        likeCount: 12,
-        isLiked:true
+        img_url: 'https://meduza.io/image/attachments/images/000/041/438/small/ZJ9sLP2Jf_vtGI2EsgDl2g.jpg',
+        like_count: 12,
+        is_like:true,
+        date_public:1478766325000,
+        user: {
+            name: "axelofan"
+        }
     },
     {   
         _id: '80ca146a814c1e89f3040c18',
         title: 'Блогеры переоделись в ниндзя, чтобы раздать подарки бездомным',
         description: 'Видеоблогер Райан Хига и его друзья переоделись в ниндзя, чтобы раздать подарки бездомным в Лас-Вегасе. Они незаметно оставляли подарочные пакеты с едой, теплой одеждой и одеялами рядом с бездомными.',
-        enclosure: 'https://meduza.io/image/attachments/images/000/041/438/small/ZJ9sLP2Jf_vtGI2EsgDl2g.jpg',
-        likeCount: 4,
-        isLiked: true,
+        img_url: 'https://meduza.io/image/attachments/images/000/041/438/small/ZJ9sLP2Jf_vtGI2EsgDl2g.jpg',
+        like_count: 4,
+        is_like: true,
+        date_public:1480603164166,
+        user: {
+            name: "axelofan"
+        }
     },
     {   
         _id: '7ebc58cbf90703c2c85ae873',
         title: 'Блогеры переоделись в ниндзя, чтобы раздать подарки бездомным',
         description: 'Видеоблогер Райан Хига и его друзья переоделись в ниндзя, чтобы раздать подарки бездомным в Лас-Вегасе. Они незаметно оставляли подарочные пакеты с едой, теплой одеждой и одеялами рядом с бездомными.',
-        enclosure: 'https://meduza.io/image/attachments/images/000/041/438/small/ZJ9sLP2Jf_vtGI2EsgDl2g.jpg',
-        likeCount: 0,
-        isLiked:false
+        img_url: 'https://meduza.io/image/attachments/images/000/041/438/small/ZJ9sLP2Jf_vtGI2EsgDl2g.jpg',
+        like_count: 0,
+        is_like:false,
+        date_public:1480599044976,
+        user: {
+            name: "axelofan"
+        }
     }
 ];
 
--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import like from './icon-like.png'
 import comment from './icon-comment.png'
 import './Post.css'
@@ -6,21 +7,22 @@ import './Post.css'
 class Post extends Component {
     constructor(props) {
         super(props);
-        this.likeCount = props.data.likeCount;
-        this.isLiked = props.data.isLiked;
+        this.state = {likeCount: props.data.like_count, isLike: props.data.is_like};
     }
 
     setLike = (e) => {
-        const likeCount = (this.isLiked) ? this.likeCount-- : this.likeCount++;
-        this.setState({likeCount: likeCount});
-        this.isLiked = !this.isLiked;
+        const likeCount = (this.state.isLike) ? this.state.likeCount-1 : this.state.likeCount+1;
+        this.setState({likeCount: likeCount, isLike: !this.state.isLike});
+    }
+    getTime() {
+        return moment(this.props.data.date_public).locale("ru").calendar();
     }
 
     render() {
         return (
             <div className='feed_col'>
                 <div className='post'>
-                    <img src={this.props.data.enclosure} alt={this.props.data.title}/>
+                    <img src={this.props.data.img_url} alt={this.props.data.title}/>
                     <div className='title'>
                         {this.props.data.title}
                     </div>
@@ -28,13 +30,17 @@ class Post extends Component {
                         {this.props.data.description}
                     </div>
                     <div className='post_footer'>
+                        <a className='username' href='#!'>{'@'+this.props.data.user.name}</a>
+                        <span className='pub_date'>{this.getTime()}</span>
+                        <div className='counters_icons'>
                         <a className='counters_like'>
-                            <img src={like} onClick={this.setLike} alt='Like' className={(this.isLiked)?'active':''}/>
-                            <span>{this.likeCount}</span>
+                            <img src={like} onClick={this.setLike} alt='Like' className={(this.state.isLike)?'active':''}/>
+                            <span>{this.state.likeCount}</span>
                         </a>
                         <a className='comments_amount'>
                             <img src={comment} alt='Comment'/>
                         </a>
+                        </div>
                     </div>
                 </div>
             </div>
