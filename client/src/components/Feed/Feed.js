07dac79557209032ee2e62439b105189ec27f28f
axelofan <cabanov.alexey@yandex.ru>
2016-12-04 16:37:29 +0400

    Сделал header сайта
    Добавил header сайта.
    Добавил имитацию входа.

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -5,14 +5,11 @@ import './Feed.css'
 
 
 class Feed extends Component {
-    constructor() {
-        super();
-        this.news = news;
-    }
+    
     render() {
         return (
             <div className='feed_row'>
-            {this.news.map(el => <Post data={el} key={el.key} />)}
+            {news.map(el => <Post data={el} key={el.key} />)}
             </div>
         )
     }
